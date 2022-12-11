665520ff32cd78f3930626a96e0b43da309aa1d3
ofek ashkenazi <ofeka18@gmail.com>
2022-12-11 18:47:02 +0200

    dropdown menu add in s m l, overall touch

--- js/modal-services.js
+++ js/modal-services.js
@@ -1,12 +1,6 @@
 'use strict'
 
-
-
-
-
-
-
-
+var gDropDownOpen = false
 
 function onOpenModal() {
     document.querySelector('.modal-data').style.display = "flex"
@@ -16,17 +10,21 @@ function onOpenModal() {
 function onCloseModal() {
     document.querySelector('.modal-data').style.display = "none"
     document.querySelector('.screen-modal').style.opacity = "0"
-   
+
 }
 
+function onDpOpen() {
+    if (!gDropDownOpen) {
+        document.querySelector('.drop-down').style.display = "block"
+        document.querySelector('ul').style.overflow = "visible"
+        gDropDownOpen = !gDropDownOpen
+    } else {
+        document.querySelector('.drop-down').style.display = "none"
+        document.querySelector('ul').style.overflow = "hidden"
+        gDropDownOpen = !gDropDownOpen
+    }
 
-
-
-
-
-
-
-
+}
 
 //  menu area
 function onToggleMenu() {
